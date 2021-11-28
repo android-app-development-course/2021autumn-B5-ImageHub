2fdab3fc0ddda2a2a9e0e521d4f777de2c431d86
Extas <694274145@qq.com>
2021-11-28 13:14:41 +0800

    添加搜索结果页的跳转

--- app/src/main/kotlin/com/hyosakura/imagehub/ui/BaseScreen.kt
+++ app/src/main/kotlin/com/hyosakura/imagehub/ui/BaseScreen.kt
@@ -3,15 +3,18 @@ package com.hyosakura.imagehub.ui
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.hyosakura.imagehub.R
 import com.hyosakura.imagehub.ui.screens.LibraryScreen
 import com.hyosakura.imagehub.ui.screens.Screen
+import com.hyosakura.imagehub.ui.screens.Screen.*
 import com.hyosakura.imagehub.ui.screens.main.MainScreen
 import com.hyosakura.imagehub.ui.screens.search.SearchResultsScreen
 import com.hyosakura.imagehub.ui.screens.search.SearchScreen
@@ -21,50 +24,93 @@ import com.hyosakura.imagehub.ui.screens.search.SearchScreen
 @Composable
 fun BaseScreen() {
 
-    val allScreens = Screen.values().toList()
+    val allScreens = values().toList()
     val navController = rememberNavController()
     val backstackEntry = navController.currentBackStackEntryAsState()
     val currentScreen = Screen.fromRoute(backstackEntry.value?.destination?.route)
 
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(title = {
-                Text(
-                    "Image Hub",
-                    style = MaterialTheme.typography.headlineLarge
-                )
-            })
+            TopBar(currentScreen)
         },
         content = { innerPadding ->
             NavHost(
                 navController = navController,
-                startDestination = Screen.Main.name,
+                startDestination = Main.name,
                 modifier = Modifier.padding(innerPadding)
             ) {
-                composable(Screen.Main.name) {
+                composable(Main.name) {
                     MainScreen()
                 }
-                composable(Screen.Search.name) {
-                    SearchScreen()
+                composable(Search.name) {
+                    SearchScreen(onSearchBarClick = { navController.navigate(SearchResults.name) })
                 }
-                composable(Screen.Library.name) {
+                composable(Library.name) {
                     LibraryScreen()
                 }
-                composable(Screen.SearchResults.name) {
+                composable(SearchResults.name) {
                     SearchResultsScreen()
                 }
             }
         },
         bottomBar = {
-            BaseBottomBar(
-                allScreens = listOf(Screen.Main, Screen.Search, Screen.Library),
-                onSelected = { screen ->
-                    navController.navigate(screen.name)
-                },
-                currentScreen = currentScreen,
-            )
+            BottonBar(navController, currentScreen)
         }
     )
+}
+
+@Composable
+private fun BottonBar(
+    navController: NavHostController,
+    currentScreen: Screen
+) {
+    when (currentScreen) {
+        Main -> BaseBottomBar(
+            allScreens = listOf(Main, Search, Library),
+            onSelected = { screen ->
+                navController.navigate(screen.name)
+            },
+            currentScreen = currentScreen,
+        )
+        Search -> BaseBottomBar(
+            allScreens = listOf(Main, Search, Library),
+            onSelected = { screen ->
+                navController.navigate(screen.name)
+            },
+            currentScreen = currentScreen,
+        )
+        Library -> BaseBottomBar(
+            allScreens = listOf(Main, Search, Library),
+            onSelected = { screen ->
+                navController.navigate(screen.name)
+            },
+            currentScreen = currentScreen,
+        )
+        SearchResults -> {
+        }
+    }
+}
+
+@Composable
+private fun TopBar(currentScreen: Screen) {
+    // TODO: 用动画实现消失效果
+    when (currentScreen) {
+        Main -> BaseTopBar()
+        Search -> BaseTopBar()
+        Library -> BaseTopBar()
+        SearchResults -> {
+        }
+    }
+}
+
+@Composable
+private fun BaseTopBar() {
+    CenterAlignedTopAppBar(title = {
+        Text(
+            stringResource(R.string.imageHub),
+            style = MaterialTheme.typography.headlineLarge
+        )
+    })
 }
 
 @Preview
--- app/src/main/kotlin/com/hyosakura/imagehub/ui/screens/search/SearchResultsScreen.kt
+++ app/src/main/kotlin/com/hyosakura/imagehub/ui/screens/search/SearchResultsScreen.kt
@@ -1,21 +1,21 @@
 package com.hyosakura.imagehub.ui.screens.search
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TabRowDefaults.Divider
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material3.*
-import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.hyosakura.imagehub.ui.theme.Purple80
+import com.hyosakura.imagehub.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,25 +25,25 @@ fun SearchResultsScreen() {
 
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Row(Modifier
-                        .clip(RoundedCornerShape(30.dp))
-                        .background(Purple80),
-                        verticalAlignment = Alignment.CenterVertically)
-                    {
-                        TextField(
-                            value = searchString,
-                            onValueChange = { searchString = it },
-                            label = { Text("搜索“标签”“注释”…") },
-                            singleLine = true
-                        )
-                    }
-                }
-            )
+            Column {
+                OutlinedTextField(
+                    value = searchString,
+                    onValueChange = { searchString = it },
+                    placeholder = { Text(stringResource(R.string.searchSuggestions), style = MaterialTheme.typography.bodyMedium, modifier = Modifier.alpha(0.7f)) },
+                    singleLine = true,
+                    leadingIcon = { Icon(painter = painterResource(R.drawable.ic_outline_search_24), contentDescription = null) },
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        textColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Divider(color = MaterialTheme.colorScheme.surfaceVariant, thickness = 1.dp)
+            }
         },
         content = {
-            Text("Page1")
+            Text("SearchResultsScreen")
         }
     )
 }
