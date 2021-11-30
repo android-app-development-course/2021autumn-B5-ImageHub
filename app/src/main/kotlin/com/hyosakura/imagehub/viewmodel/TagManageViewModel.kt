9797bd519e5faf83d2028043c39d2c315fce57aa
LovesAsuna <qq625924077@gmail.com>
2021-11-30 16:02:28 +0800

    :sparkles: feat(viewmodel): complete TagManageViewModel

--- app/src/main/kotlin/com/hyosakura/imagehub/viewmodel/TagManageViewModel.kt
+++ app/src/main/kotlin/com/hyosakura/imagehub/viewmodel/TagManageViewModel.kt
@@ -2,13 +2,23 @@ package com.hyosakura.imagehub.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import com.hyosakura.imagehub.repository.DataRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 class TagManageViewModel(private val repository: DataRepository) : ViewModel() {
+    val allTags = repository.getAllTags().asLiveData()
 
+    fun imageNumInTag(tagId: Int): Int {
+        return runBlocking {
+            repository.imageNumInTag(tagId).first().images.size
+        }
+    }
 }
 
-class TagManageViewModelFactory(private val repository: DataRepository) : ViewModelProvider.Factory {
+class TagManageViewModelFactory(private val repository: DataRepository) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TagManageViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
