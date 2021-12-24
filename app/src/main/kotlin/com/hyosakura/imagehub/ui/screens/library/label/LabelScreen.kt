6424d14786ab8dded2e28d9949799ffb3ffedaeb
LovesAsuna <qq625924077@gmail.com>
2021-12-24 10:09:54 +0800

    :recycle: refactor(ViewModel): refactor

--- app/src/main/kotlin/com/hyosakura/imagehub/ui/screens/library/label/LabelScreen.kt
+++ app/src/main/kotlin/com/hyosakura/imagehub/ui/screens/library/label/LabelScreen.kt
@@ -1,6 +1,14 @@
 package com.hyosakura.imagehub.ui.screens.library.label
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import com.hyosakura.imagehub.entity.toDateTime
@@ -16,6 +24,31 @@ fun LabelScreen(
         TagManageViewModel::class.java
     )
 ) {
+    TopAppBar(
+        title = { Text("这是标题") },
+        navigationIcon = {
+            IconButton(onClick = { }) {
+                Icon(Icons.Filled.ArrowBack, null)
+            }
+        },
+        actions = {
+            IconButton(onClick = {
+                viewModel.insertTag("tag名")
+            }) {
+                Icon(Icons.Filled.Share, "添加")
+            }
+            IconButton(onClick = {
+                // viewModel.updateTag()
+            }) {
+                Icon(Icons.Filled.Settings, "修改")
+            }
+            IconButton(onClick = {
+                // viewModel.deleteTag()
+            }) {
+                Icon(Icons.Filled.Settings, "删除")
+            }
+        }
+    )
     Column {
         viewModel.allTags.observeAsState().value?.let { entityList ->
             val map = entityList.stream().collect(Collectors.groupingBy {
